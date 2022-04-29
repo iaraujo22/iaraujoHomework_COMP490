28c2aab5ddf4870c0bc0ea0d0cfc750ac2fd53a5
iaraujo22 <iaraujo@student.bridgew.edu>
2022-04-28 20:39:03 -0400

    More tests

--- test/test_total_calculation.py
+++ test/test_total_calculation.py
@@ -5,6 +5,8 @@ shop = [total_charge.ItemRecord('cereal', 10.00, 'wic')]
 
 shop2 = [total_charge.ItemRecord('shoes', 165.00, 'clothes')]
 
+shop3 = [total_charge.ItemRecord('None', 00.00, 'none')]
+
 
 # happy path
 def test_total_charge():
@@ -21,13 +23,22 @@ def test_total_charge3():
     result = total_charge.calculate_total('Maine', shop)
     assert result == 10.00
 
-#////////////////////////////////////////////////////////////////////////
+
+# ////////////////////////////////////////////////////////////////////////
 # testing clothes MA and ME
 def test_total_clothes():
     result = total_charge.calculate_total('Maine', shop2)
-    assert result == 174.075
+    assert result == 174.07
 
 
 def test_total_clothes2():
     result = total_charge.calculate_total('Massachusetts', shop2)
     assert result == 165.00
+
+
+# ////////////////////////////////////////////////////////////////////////
+# testing BAD DATA
+
+def test_empty_cart():
+    result = total_charge.calculate_total('Massachusetts', shop3)
+    assert result == 0
--- total_charge.py
+++ total_charge.py
@@ -13,29 +13,36 @@ def calculate_total(state: str, currentItemRecord: list[ItemRecord]):
     tax = 0.0
     if state == 'Massachusetts':
         for item in currentItemRecord:
+            if item.price <= 0:
+                raise ValueError
             tax = item.price * 0.0625
             if item.item_type == 'wic':
                 total_to_charge = item.price
             elif item.item_type == 'clothes' and item.price > 175.00:
-                total_to_charge = item.price + tax
+                total_to_charge = round(item.price + tax, 2)
             elif item.item_type == 'clothes':
-                total_to_charge = item.price
+                total_to_charge = round(item.price, 2)
             else:
-                total_to_charge = item.price + tax
+                total_to_charge = round(item.price + tax, 2)
     elif state == 'New Hampshire':
         for item in currentItemRecord:
+            if item.price <= 0:
+                raise ValueError
             total_to_charge = item.price
     elif state == 'Maine':
         for item in currentItemRecord:
+            if item.price <= 0:
+                raise ValueError
             if item.item_type == 'wic':
                 total_to_charge = item.price
             else:
-                tax_maine = item.price * 0.055
-                total_to_charge = item.price + tax_maine
+                tax_maine = round(item.price * 0.055, 2)
+                total_to_charge = round(item.price + tax_maine, 2)
 
+    total_to_charge = round(total_to_charge, 2)
     return total_to_charge
 
 
-shop2 = [ItemRecord('shoes', 165.00, 'clothes')]
+#shop2 = [ItemRecord('shoes', 00.00, 'clothes')]
 
-print("Your Total charge is: $ {} ".format(calculate_total('Maine', shop2)))
+#print("Your Total charge is: $ {} ".format(calculate_total('Maine', shop2)))
